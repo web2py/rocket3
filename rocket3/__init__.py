ff59ae9f9d188ab6c62ecf44feea9e7f70fdcca6
unknown <valq7711@gmail.com>
2021-02-08 23:04:46 +0300

    isAlive -> is_alive

--- rocket3/__init__.py
+++ rocket3/__init__.py
@@ -500,7 +500,7 @@ class Listener(threading.Thread):
             self.err_log.warning("Listener started when not ready.")
             return
 
-        if self.thread is not None and self.thread.isAlive():
+        if self.thread is not None and self.thread.is_alive():
             self.err_log.warning("Listener already running.")
             return
 
@@ -508,11 +508,11 @@ class Listener(threading.Thread):
 
         self.thread.start()
 
-    def isAlive(self):
+    def is_alive(self):
         if self.thread is None:
             return False
 
-        return self.thread.isAlive()
+        return self.thread.is_alive()
 
     def join(self):
         if self.thread is None:
@@ -675,14 +675,14 @@ class Rocket3:
         if background:
             return
 
-        while self._monitor.isAlive():
+        while self._monitor.is_alive():
             try:
                 time.sleep(THREAD_STOP_CHECK_INTERVAL)
             except KeyboardInterrupt:
                 # Capture a keyboard interrupt when running from a console
                 break
             except:
-                if self._monitor.isAlive():
+                if self._monitor.is_alive():
                     log.error(traceback.format_exc())
                     continue
 
@@ -702,12 +702,12 @@ class Rocket3:
             time.sleep(0.01)
 
             for l in self.listeners:
-                if l.isAlive():
+                if l.is_alive():
                     l.join()
 
             # Stop Monitor
             self._monitor.stop()
-            if self._monitor.isAlive():
+            if self._monitor.is_alive():
                 self._monitor.join()
 
             # Stop Worker threads
@@ -970,14 +970,14 @@ class ThreadPool:
             self.app_info["executor"].shutdown(wait=False)
 
         # Give them the gun
-        # active_threads = [t for t in self.threads if t.isAlive()]
+        # active_threads = [t for t in self.threads if t.is_alive()]
         # while active_threads:
         #     t = active_threads.pop()
         #     t.kill()
 
         # Wait until they pull the trigger
         for t in self.threads:
-            if t.isAlive():
+            if t.is_alive():
                 t.join()
 
         # Clean up the mess
@@ -986,7 +986,7 @@ class ThreadPool:
     def bring_out_your_dead(self):
         # Remove dead threads from the pool
 
-        dead_threads = [t for t in self.threads if not t.isAlive()]
+        dead_threads = [t for t in self.threads if not t.is_alive()]
         for t in dead_threads:
             if __debug__:
                 log.debug("Removing dead thread: %s." % t.getName())
